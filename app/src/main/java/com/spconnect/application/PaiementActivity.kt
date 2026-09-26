6da5374aa305afa1ca4448d487ebf4fc32835a2e
contacterserviceplus-eng <contacterserviceplus@gmail.com>
2026-09-26 21:41:38 +0100

    Create PaiementActivity.kt

---
+++ app/src/main/java/com/spconnect/application/PaiementActivity.kt
@@ -0,0 +1,11 @@
+package com.spconnect.application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaiementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paiement)
+    }
+}
